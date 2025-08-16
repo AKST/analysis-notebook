f8c82a4f2a59bc97ae40b809215936ec4978d128
akst <contact@akst.io>
2025-08-17 00:25:40 +1000

    updated externality & public good section

--- lib/app/sec-1101/sec-09/type.ts
+++ lib/app/sec-1101/sec-09/type.ts
@@ -1,13 +1,28 @@
+import { RenderContextInit, Econ } from '../../prelude-type.ts';
+import { ModelRenderer } from '../common/2d/model.js';
+
+export type RenderContext = RenderContextInit & {
+  model: ModelRenderer,
+}
+
 export type CoaseProblem = {
   initator: { refrain: number, exercise: number },
   bystander: { refrain: number, exercise: number },
 };
 
+type MarketVariant<V> = {
+  readonly demand: V;
+  readonly supply: V;
+};
+
 export type Config = {
-  readonly placeholder: number;
+  readonly externality: number;
+  readonly transfer: number;
+  readonly market: MarketVariant<[number, number]>;
 };
 
 export type State = {
+  readonly model?: [Econ.Model.Config.T,  Econ.Model.Desc.T];
 };
 
 export type Event =
