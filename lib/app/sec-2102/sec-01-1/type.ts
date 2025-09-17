a55da18f02be33a6bef069af91dc75bc461ffa04
akst <contact@akst.io>
2025-09-18 01:32:59 +1000

    wrote out notes for lecture 1

--- lib/app/sec-2102/sec-01-1/type.ts
+++ lib/app/sec-2102/sec-01-1/type.ts
@@ -18,6 +18,7 @@ export type Event =
 
 export type Config = {
   productivity: {
+    technology: number,
     capital: number,
     labour: number,
     alpha: number,
@@ -26,14 +27,40 @@ export type Config = {
 
 export type Chart2d = {
   bounds: Vec<'r', 2>,
-  points: readonly Vec<'r', 2>[]
+  points: readonly Vec<'r', 2>[],
+  tick: Vec<'r', 2> | number,
+};
+
+export type ComparisonChart2d = {
+  bounds: Vec<'r', 2>,
+  tick: Vec<'r', 2> | number,
+  points: Record<string, {
+    points: readonly Vec<'r', 2>[],
+    colour: number,
+  }>,
+};
+
+export type EquilibriumChart2d = {
+  bounds: Vec<'r', 2>,
+  points: readonly Vec<'r', 2>[],
+  tick: Vec<'r', 2> | number,
+  eq: Vec<'r', 2>,
 };
 
 export type State = {
+  aggregate: {
+    output: number,
+    realWage: number,
+    realRentOfCapital: number,
+  },
   charts: {
     productivityOverLabour?: Chart2d,
     productivityOverCapital?: Chart2d,
-    productivityOverTechnology?: Chart2d,
+    marginalProductOfLabor?: Chart2d,
+    marginalProductOfCapital?: Chart2d,
+    equilibriumLabour?: EquilibriumChart2d,
+    equilibriumCapital?: EquilibriumChart2d,
+    usaVsChina?: ComparisonChart2d,
   },
 };
 
