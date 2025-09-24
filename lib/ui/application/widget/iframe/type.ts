0440707084600f57433579331d9ae19aa149a382
akst <contact@akst.io>
2025-09-24 15:27:45 +1000

    added iframe message normalisation

--- lib/ui/application/widget/iframe/type.ts
+++ lib/ui/application/widget/iframe/type.ts
@@ -5,10 +5,8 @@ export type IframeWidget<Ctx, State, Config> = {
   title: string,
   path: string,
   header?: E.Item | undefined;
+  normaliseMessage?(message: ParentToChildMessage<Config, State>): Generator<any>;
 };
 
 export type ParentToChildMessage<Cfg, St> =
-  | { kind: 'init' }
-  | { kind: 'push-render', config: Cfg, state: St }
-  | { kind: 'config', config: Cfg }
-  | { kind: 'state', config: St };
+  | { kind: 'push', config: Cfg, state: St }
