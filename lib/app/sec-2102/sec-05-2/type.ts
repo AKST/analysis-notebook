52afcda5f29924e31ce1fb26b5410e4db9c9fdcf
akst <contact@akst.io>
2025-10-25 18:03:16 +1100

    setup knobs for IS Curve

--- lib/app/sec-2102/sec-05-2/type.ts
+++ lib/app/sec-2102/sec-05-2/type.ts
@@ -16,7 +16,24 @@ export type RenderContext = RenderContextInit & {
 export type Event =
   | { kind: 'config', config: Config };
 
+export type CobbDouglas = { labour: number, capital: number, ftp: number, alpha: number };
+
 export type Config = {
+  mpk: (
+    | { kind: 'exogenous', value: number }
+    | { kind: 'cobbDouglas', value: CobbDouglas }
+  ),
+  realInterest: number,
+  isCurve: {
+    aggregateConsumption: number;
+    aggregateGovtSpend: number;
+    aggregateImporting: number;
+    aggregateExporting: number,
+    investment: {
+      aggregate: number;
+      rateSensitivity: number;
+    };
+  },
 };
 
 
