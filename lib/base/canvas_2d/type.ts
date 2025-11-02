9a93b6c429c038144ea0828a8e0bda341fa90dc1
akst <contact@akst.io>
2025-11-02 18:40:09 +1100

    add more text renderering options

--- lib/base/canvas_2d/type.ts
+++ lib/base/canvas_2d/type.ts
@@ -1,25 +1,29 @@
 import { ComplexViewport } from './viewport.js';
 
 export interface Renderer {
-  setFillStyle(color: number): void;
-  setStrokeStyle(color: number): void;
-  setLineWidth(width: number): void;
-  setLineDash(lineDash: [number, number, ...number[]] | []): void;
-  setGlobalCompositeOperation(mode: GlobalCompositeOperation): void;
-  getGlobalCompositeOperation(): GlobalCompositeOperation;
-  fillRect(x: number, y: number, width: number, height: number): void;
-  beginPath(): void;
   arc(x: number, y: number, radius: number, startAngle: number, endAngle: number): void;
   fill(): void;
+  setFillStyle(color: number): void;
+  setFont(font: string): void;
+  setGlobalCompositeOperation(mode: GlobalCompositeOperation): void;
+  getGlobalCompositeOperation(): GlobalCompositeOperation;
+  setLineWidth(width: number): void;
+  setLineDash(lineDash: [number, number, ...number[]] | []): void;
+  beginPath(): void;
+  closePath(): void;
+  fillRect(x: number, y: number, width: number, height: number): void;
+  rotate(n: number): void;
+  stroke(): void;
+  setStrokeStyle(color: number): void;
   moveTo(x: number, y: number): void;
   lineTo(x: number, y: number): void;
-  closePath(): void;
-  stroke(): void;
-  setFont(font: string): void;
   fillText(text: string, x: number, y: number): void;
   setTextAlign(align: CanvasTextAlign): void;
   setTextBaseline(baseline: CanvasTextBaseline): void;
   measureText(text: string): TextMetrics;
+  getTransform(): DOMMatrix;
+  setTransform(matrix: DOMMatrix): void;
+  translate(x: number, y: number): void;
 }
 
 export type ProjectionConstraint =
