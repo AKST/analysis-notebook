c9fea8b465317f41da2a10d7c1d493c811803e32
akst <contact@akst.io>
2025-12-14 22:01:21 +1100

    use builders in underlying dsl

--- lib/base/dom_app/helper/type.ts
+++ lib/base/dom_app/helper/type.ts
@@ -3,7 +3,7 @@ import { E, ElAttributes } from '../type.ts'
 export type ChildOf<L> = L extends (infer I)[] ? I : never;
 export type ChildOfIfEl<L> = L extends E.Item[] ? E.Item : never;
 
-interface AttributeBuilder<Attrs> {
+export interface AttributeBuilder<Attrs> {
 }
 
 export type VoidConstructor<O> = (attrs: O) => E.Item;
@@ -14,78 +14,63 @@ export interface VoidBuilderAbstract<Attrs> extends AttributeBuilder<Attrs> {
 
 export type HelperConstructor<O, I> = (attrs: O, items: I) => E.Item;
 
-export type BuilderAbstract<Attrs, I> = AttributeBuilder<Attrs> & {
-  attr: (attr: Attrs) => BuilderAbstract<Attrs, I>;
-  of: (...elements: ChildOf<I>[]) => E.Item;
-  pre: (element: ChildOf<I>) => BuilderAbstract<Attrs, I>;
-  app: (element: ChildOf<I>) => BuilderAbstract<Attrs, I>;
-  post: (element: ChildOf<I>) => BuilderAbstract<Attrs, I>;
-} & (I extends E.Item[] ? TextBuilderAbstract<Attrs> : {});
+type OverloadBuilderAbstract<Attrs, I> =
+  I extends E.Item[]
+    ? TextBuilderAbstract<Attrs, I>
+    : BuilderAbstract<Attrs, I>;
 
-export type TextBuilderAbstract<Attrs> = {
+export type BuilderAbstract<Attrs, I> = AttributeBuilder<Attrs> & {
+  attr: (attr: Attrs) => OverloadBuilderAbstract<Attrs, I>;
+  of: (...elements: ChildOf<I>[]) => E.Item;
+  pre: (element: ChildOf<I>) => OverloadBuilderAbstract<Attrs, I>;
+  app: (element: ChildOf<I>) => OverloadBuilderAbstract<Attrs, I>;
+  post: (element: ChildOf<I>) => OverloadBuilderAbstract<Attrs, I>;
+};
+
+export type TextBuilderAbstract<Attrs, I> = BuilderAbstract<Attrs, I> & {
   t: (text: TemplateStringsArray, ...items: E.Item[]) => E.Item;
-  preT: (text: TemplateStringsArray, ...items: E.Item[]) => BuilderAbstract<Attrs, E.Item[]>;
-  appT: (text: TemplateStringsArray, ...items: E.Item[]) => BuilderAbstract<Attrs, E.Item[]>;
-  postT: (text: TemplateStringsArray, ...items: E.Item[]) => BuilderAbstract<Attrs, E.Item[]>;
+  preT: (text: TemplateStringsArray, ...items: E.Item[]) => OverloadBuilderAbstract<Attrs, I>;
+  appT: (text: TemplateStringsArray, ...items: E.Item[]) => OverloadBuilderAbstract<Attrs, I>;
+  postT: (text: TemplateStringsArray, ...items: E.Item[]) => OverloadBuilderAbstract<Attrs, I>;
 };
 
 type GeneralVoidHelper<Attrs> = VoidBuilderAbstract<Attrs> & {
   (attr?: Attrs): E.Item;
 };
 
-type GeneralTemplateHelper<Attrs, Item> = {
-  (text: TemplateStringsArray, ...items: Item[]): E.Item;
-  (attr: Attrs): {
-    t: (text: TemplateStringsArray, ...items: Item[]) => E.Item;
-    of: (...items: Item[]) => E.Item;
-  };
-  attr: (attr: Attrs) => {
-    t: (text: TemplateStringsArray, ...items: Item[]) => E.Item;
-    of: (...items: Item[]) => E.Item;
-  };
-  of: (...items: Item[]) => E.Item;
-};
-
-type New_GeneralHelper<Attrs, Items> = Items extends any[] ? (BuilderAbstract<Attrs, Items> & {
-  (...items: Items): E.Item;
-  (attr: Attrs): BuilderAbstract<Attrs, Items>;
+type GeneralTemplateHelper<Attrs, Items> = Items extends any[] ? (OverloadBuilderAbstract<Attrs, Items> & {
+  (text: TemplateStringsArray, ...items: Items): E.Item;
+  (attr: Attrs): OverloadBuilderAbstract<Attrs, Items>;
 }) : never;
 
-type GeneralHelper<Attrs, Items> = Items extends any[] ? {
+type GeneralHelper<Attrs, Items> = Items extends any[] ? (OverloadBuilderAbstract<Attrs, Items> & {
   (...items: Items): E.Item;
-  (attr: Attrs): {
-    of: (...items: Items) => E.Item;
-  };
-  attr(attr: Attrs): {
-    of: (...items: Items) => E.Item;
-  };
-  of: (...items: Items) => E.Item;
-} : never;
+  (attr: Attrs): OverloadBuilderAbstract<Attrs, Items>;
+}) : never;
 
 export type VoidHelper<O = undefined> =
   O extends undefined ?
   GeneralVoidHelper<ElAttributes> :
   GeneralVoidHelper<O & ElAttributes>;
 
-export type TemplateHelper<O = undefined, Item = E.Item> =
+export type TemplateHelper<O = undefined, Items = E.Item[]> =
   O extends undefined ?
-  GeneralTemplateHelper<ElAttributes, Item> :
-  GeneralTemplateHelper<O & ElAttributes, Item>;
+  GeneralTemplateHelper<ElAttributes, Items> :
+  GeneralTemplateHelper<O & ElAttributes, Items>;
 
 export type Helper<O = undefined, Items = E.Item[]> =
   O extends undefined ?
   GeneralHelper<ElAttributes, Items> :
   GeneralHelper<O & ElAttributes, Items>;
 
-
-export type TemplateHelperFactory = {
-  <O extends Object, Item = E.Item>(f: (attrs: O & ElAttributes, items: Item[]) => E.Item, defaultArgs: O): TemplateHelper<O, Item>;
-  <Item = E.Item>(f: (attrs: ElAttributes, items: Item[]) => E.Item): TemplateHelper<undefined, Item>;
-}
-
 export type VoidHelperFactory = {
   <O extends Object>(f: (attrs: O & ElAttributes) => E.Item, defaultArgs: O): VoidHelper<O>;
   (f: (attrs: ElAttributes) => E.Item): VoidHelper<undefined>;
+}
+
+export type TemplateHelperFactory = {
+  <O extends Object, Items extends any[] = E.Item[]>(f: (attrs: O & ElAttributes, items: Items) => E.Item, defaultArgs: O): TemplateHelper<O, Items>;
+  <Items extends any[] = E.Item[]>(f: (attrs: ElAttributes, items: Items) => E.Item): TemplateHelper<undefined, Items>;
 }
 
 export type HelperFactory = {
